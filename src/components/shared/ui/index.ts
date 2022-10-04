3ae723275f4b7cca2af1150cfc6213c0e3432cd1
Guido Cerioni <guido.cerioni@radiumrocket.com>
2022-10-04 16:24:46 -0300

    RL-19: Setup Table component

--- src/components/shared/ui/index.ts
+++ src/components/shared/ui/index.ts
@@ -3,4 +3,5 @@ export { default as Dropdown } from './inputs/dropdown';
 export { default as InputText } from './inputs/text';
 export { default as Modal } from './modal';
 export { default as Preloader } from './preloader';
+export { default as Table } from './table';
 export { default as Text } from './text/text';
