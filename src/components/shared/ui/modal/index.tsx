6374a6b2f720bdf2e215f2cbad199c29c957ffcb
iaracriscenti <iara.criscenti@radiumrocket.com>
2022-10-04 20:44:48 -0300

    RL-16: Creation of modal component

---
+++ src/components/shared/ui/modal/index.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+
+import { ModalProps } from './types';
+
+const Modal = ({ handleClose, handleConfirm, title, content, type, ...props }: ModalProps) => {
+  return (
+    <Dialog
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+      {...props}
+    >
+      <DialogTitle sx={{ px: 5, pt: 5 }}>{title}</DialogTitle>
+      <DialogContent sx={{ p: 5, width: 480, height: 100 }}>{content}</DialogContent>
+      <DialogActions sx={{ p: 5 }}>
+        {type == 'confirm' && (
+          <>
+            <Button variant="outlined" size="large" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="contained" size="large" onClick={handleConfirm}>
+              Confirm
+            </Button>
+          </>
+        )}
+        {type == 'alert' && (
+          <Button variant="contained" onClick={handleClose}>
+            Continue
+          </Button>
+        )}
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default Modal;
