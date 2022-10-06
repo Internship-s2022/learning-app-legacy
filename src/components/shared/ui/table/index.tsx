78778c0c47e0f6b69bd9bf003390f8c1ab34b454
Guido Cerioni <guido.cerioni@radiumrocket.com>
2022-10-06 17:48:35 -0300

    RL-19: Add Table component

--- src/components/shared/ui/table/index.tsx
+++ src/components/shared/ui/table/index.tsx
@@ -1,27 +1,220 @@
 import React from 'react';
-import { FieldValues } from 'react-hook-form';
-import Box from '@mui/material/Box';
-import { DataGrid } from '@mui/x-data-grid';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import {
+  alpha,
+  Box,
+  Checkbox,
+  FormControlLabel,
+  Paper,
+  Switch,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Toolbar,
+} from '@mui/material';
 
-import { TableProps } from './types';
+import { CustomTableProps, CustomTableToolbarProps, TableProps } from './types';
 
-const Table = <TTableRowType extends FieldValues>({
-  columns,
-  rows,
-}: TableProps<TTableRowType>): JSX.Element => {
+const CustomTableHead = (props: CustomTableProps) => {
+  const { onSelectAllClick, numSelected, rowCount, headCells, icons } = props;
+
   return (
-    <Box sx={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-        disableSelectionOnClick
-        experimentalFeatures={{ newEditingApi: true }}
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              'aria-label': 'select all desserts',
+            }}
+          />
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+          >
+            {headCell.label}
+          </TableCell>
+        ))}
+        {icons && <TableCell>Actions</TableCell>}
+      </TableRow>
+    </TableHead>
+  );
+};
+
+const CustomTableToolbar = (props: CustomTableToolbarProps) => {
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
+        }),
+      }}
+    >
+      {numSelected > 0 ? <p>title</p> : <p>title</p>}
+    </Toolbar>
+  );
+};
+
+const CustomTable = <TData extends { _id: string }>({
+  headCells,
+  rows,
+  icons,
+  handleDelete,
+  handleEdit,
+}: TableProps<TData>): JSX.Element => {
+  const [selected, setSelected] = React.useState<readonly string[]>([]);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const newSelected = rows.map((n) => n._id);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event: React.MouseEvent<unknown>, _id: string) => {
+    const selectedIndex = selected.indexOf(_id);
+    let newSelected: readonly string[] = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, _id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDense(event.target.checked);
+  };
+
+  const isSelected = (name: string) => selected.indexOf(name) !== -1;
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <CustomTableToolbar numSelected={selected.length} />
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+          >
+            <CustomTableHead
+              headCells={headCells}
+              numSelected={selected.length}
+              onSelectAllClick={handleSelectAllClick}
+              rowCount={rows.length}
+              icons={icons}
+            />
+            <TableBody>
+              {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+              rows.slice().sort(getComparator(order, orderBy)) */}
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row._id);
+                  const labelId = `Custom-table-checkbox-${index}`;
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row._id)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={index}
+                      selected={isItemSelected}
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox
+                          color="primary"
+                          checked={isItemSelected}
+                          inputProps={{
+                            'aria-labelledby': labelId,
+                          }}
+                        />
+                      </TableCell>
+                      {/* <TableCell align="right">b</TableCell> */}
+
+                      {headCells.map((headCell, index) => {
+                        return <TableCell key={index}>{row[headCell.id]}</TableCell>;
+                      })}
+                      {icons && (
+                        <TableCell>
+                          <EditIcon />
+                          <DeleteIcon />
+                        </TableCell>
+                      )}
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: (dense ? 33 : 53) * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
       />
     </Box>
   );
 };
 
-export default Table;
+export default CustomTable;
--- src/components/shared/ui/table/types.ts
+++ src/components/shared/ui/table/types.ts
@@ -1,6 +1,24 @@
-import { GridColDef } from '@mui/x-data-grid';
+export interface HeadCell<DataType> {
+  disablePadding: boolean;
+  id: keyof DataType;
+  label: string;
+  numeric: boolean;
+}
+export interface TableProps<TRowData> {
+  headCells: any;
+  rows: TRowData[];
+  icons?: boolean;
+  handleDelete?: any;
+  handleEdit?: any;
+}
+export interface CustomTableProps {
+  numSelected: number;
+  headCells: [any];
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  rowCount: number;
+  icons: boolean;
+}
 
-export interface TableProps<TRowType> {
-  rows: Array<TRowType>;
-  columns: GridColDef[];
+export interface CustomTableToolbarProps {
+  numSelected: number;
 }
