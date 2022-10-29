02615e2b0c78a2404e594a6c2b052cfe4a7c023b
Franco Marini <franco.marini@radiumrocket.com>
2022-10-29 15:30:55 -0300

    RL-12: Clear store when logout

--- src/redux/modules/index.ts
+++ src/redux/modules/index.ts
@@ -1,16 +1,23 @@
 import { combineReducers } from 'redux';
 
 import authReducer from './auth/reducer';
+import { Actions } from './auth/types';
 import { postulantReducer } from './postulant';
 import { RootAction, RootReducer } from './types';
 import uiReducer from './ui/reducer';
 import { userReducer } from './user';
 
-const rootReducer = combineReducers<RootReducer, RootAction>({
+const appReducer = combineReducers<RootReducer, RootAction>({
   user: userReducer,
   postulant: postulantReducer,
   ui: uiReducer,
   auth: authReducer,
 });
+
+const rootReducer = (state, action) => {
+  // Clear all data in redux store to initial.
+  if (action.type === Actions.LOGOUT_SUCCESS) state = undefined;
+  return appReducer(state, action);
+};
 
 export default rootReducer;
