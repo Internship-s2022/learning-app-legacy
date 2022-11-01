5f0135948f02e9b810cf59fa9aded91dc4e914bf
Franco Marini <franco.marini@radiumrocket.com>
2022-11-01 11:41:14 -0300

    RL-12: Fix tab redirection on click the logo

--- src/components/shared/common/header/index.tsx
+++ src/components/shared/common/header/index.tsx
@@ -1,9 +1,9 @@
 import { ThunkDispatch } from 'redux-thunk';
-import * as React from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import LogoutIcon from '@mui/icons-material/Logout';
-import { AppBar, Box, Button, IconButton, Tabs, Toolbar, Tooltip } from '@mui/material';
+import { AppBar, Box, Button, IconButton, Toolbar, Tooltip } from '@mui/material';
 
 import { images } from 'src/assets';
 import { HeaderTabs } from 'src/components/shared/ui';
@@ -14,57 +14,52 @@ import { RootAction, RootReducer } from 'src/redux/modules/types';
 import styles from './header.module.css';
 import { HeaderProps } from './types';
 
+const superAdminHeader = [
+  { label: SuperAdminRoutes.users.label, route: SuperAdminRoutes.users.route },
+  { label: SuperAdminRoutes.courses.label, route: SuperAdminRoutes.courses.route },
+];
+
 const Header = ({ routes, logoutOption }: HeaderProps) => {
   const dispatch = useDispatch<ThunkDispatch<RootReducer, null, RootAction>>();
   const history = useNavigate();
   const { authenticated } = useSelector((state: RootReducer) => state.auth);
-  const [value, setValue] = React.useState(0);
-  const superAdminHeader = [
-    { label: SuperAdminRoutes.users.label, route: SuperAdminRoutes.users.route },
-    { label: SuperAdminRoutes.courses.label, route: SuperAdminRoutes.courses.route },
-  ];
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
 
   return (
     <AppBar>
       <div className={styles.container}>
-        <Tabs value={value} onChange={handleChange}>
-          <Toolbar disableGutters className={styles.toolBar}>
-            <Box className={styles.navTabBox}>
-              <Link to={''}>
-                <IconButton color="inherit">
-                  <img src={images.rocketLogoTab.imagePath} alt={images.rocketLogoTab.alt} />
-                </IconButton>
+        <Toolbar disableGutters className={styles.toolBar}>
+          <Box className={styles.navTabBox}>
+            <Link to="">
+              <IconButton color="inherit">
+                <img src={images.rocketLogoTab.imagePath} alt={images.rocketLogoTab.alt} />
+              </IconButton>
+            </Link>
+            {authenticated?.userType === 'SUPER_ADMIN' ? (
+              <HeaderTabs elements={superAdminHeader} />
+            ) : null}
+          </Box>
+          <Box className={styles.authBox}>
+            {logoutOption && (
+              <Tooltip title="Log Out">
+                <Button
+                  variant="text"
+                  endIcon={<LogoutIcon />}
+                  onClick={() => {
+                    dispatch(logout());
+                    history(HomeRoutes.login.route);
+                  }}
+                >
+                  Salir
+                </Button>
+              </Tooltip>
+            )}
+            {!logoutOption && routes?.login?.label && (
+              <Link to={routes.login.route} key={routes.login.label}>
+                <Button key={routes.login.label}>{routes.login.label}</Button>
               </Link>
-              {authenticated?.userType === 'SUPER_ADMIN' ? (
-                <HeaderTabs elements={superAdminHeader} />
-              ) : null}
-            </Box>
-            <Box className={styles.authBox}>
-              {logoutOption && (
-                <Tooltip title="Log Out">
-                  <Button
-                    variant="text"
-                    endIcon={<LogoutIcon />}
-                    onClick={() => {
-                      dispatch(logout());
-                      history(HomeRoutes.login.route);
-                    }}
-                  >
-                    Salir
-                  </Button>
-                </Tooltip>
-              )}
-              {!logoutOption && routes?.login?.label && (
-                <Link to={routes.login.route} key={routes.login.label}>
-                  <Button key={routes.login.label}>{routes.login.label}</Button>
-                </Link>
-              )}
-            </Box>
-          </Toolbar>
-        </Tabs>
+            )}
+          </Box>
+        </Toolbar>
       </div>
     </AppBar>
   );
