57776b66e1bc0a0d641fdec548034910ea813214
Guido Cerioni <guido.cerioni@radiumrocket.com>
2022-11-01 15:28:40 -0300

    RL-87: implement Tabs

---
+++ src/components/pages/super-admin/courses/admission-tests-list/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box } from '@mui/system';
+
+import { Text } from 'src/components/shared/ui';
+
+const AdmissionTestsList = () => {
+  return (
+    <Box>
+      <Text variant="h1">Admission tests</Text>
+    </Box>
+  );
+};
+
+export default AdmissionTestsList;
