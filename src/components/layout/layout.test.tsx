6d5a47c72098d0217380dbff9141bb007b76ea2c
iaracriscenti <iara.criscenti@radiumrocket.com>
2022-11-03 17:13:59 -0300

    RL-144: Update layout test

--- src/components/layout/layout.test.tsx
+++ src/components/layout/layout.test.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { cleanup, render } from '@testing-library/react';
 
-import store from 'src/redux/store';
-import AppRoutes from 'src/routes';
+import { render } from 'src/utils/custom-render';
 
-afterEach(() => {
-  cleanup();
-});
-
-beforeEach(() => {
-  cleanup();
-});
+import Layout from '.';
 
 describe('Layout Component', () => {
-  test.skip('Layout Rendering', () => {
-    const { getByTestId } = render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
-      </Provider>,
-    );
-    expect(getByTestId('layout-container-div')).toBeInTheDocument();
+  const customChildren = <div data-testid="children"></div>;
+
+  it('Should render the layout and header containers', () => {
+    const { queryByTestId } = render(<Layout />);
+    expect(queryByTestId('layout-container-div')).toBeInTheDocument();
+    expect(queryByTestId('header-container-div')).toBeInTheDocument();
+    expect(queryByTestId('sidebar-container-div')).not.toBeInTheDocument();
+    expect(queryByTestId('footer-container-div')).not.toBeInTheDocument();
+  });
+
+  it('Should render the sidebar when passing the sidebarOn prop', () => {
+    const { queryByTestId } = render(<Layout sidebarOn />);
+    expect(queryByTestId('sidebar-container-div')).toBeInTheDocument();
+  });
+
+  it('Should render the footer when passing the footerOn prop', () => {
+    const { queryByTestId } = render(<Layout footerOn />);
+    expect(queryByTestId('footer-container-div')).toBeInTheDocument();
+  });
+
+  it('Should render a children when wrapping one', () => {
+    const { queryByTestId } = render(<Layout>{customChildren}</Layout>);
+    expect(queryByTestId('children')).toBeInTheDocument();
   });
 });
