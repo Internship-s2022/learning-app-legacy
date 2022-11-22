7c6f9de57a1ab504157249a94a144b395d2e677d
Julián Demeglio <julian.demeglio@radiumrocket.com>
2022-11-22 12:37:46 -0300

    RL-178: Small update for rebase

--- test/specs/super-admin/course-creation-happyPath.js
+++ test/specs/super-admin/course-creation-happyPath.js
@@ -14,40 +14,6 @@ describe('Superadmin add Course E2E happy path', () => {
       browser.url('/login');
     });
 
-    it('Verify layout shared component', async () => {
-      await expect(layout.layoutContainer).not.toBeDisplayed();
-    });
-
-    it('Verify Radium Learning Logo', async () => {
-      await expect(logInPage.logoContainer).toBeDisplayed();
-      await expect(logInPage.logoIconRR).toBeDisplayed();
-      await expect(logInPage.logoTextRR).toBeDisplayed();
-    });
-    it('Verify welcome messages', async () => {
-      await expect(logInPage.WelcomeMsgContainer).toBeDisplayed();
-      await expect(logInPage.WelcomeMsgTittle).toBeDisplayed();
-      await expect(logInPage.WelcomeMsgText).toBeDisplayed();
-    });
-    it('Verify login fields', async () => {
-      await logInPage.login('@mail.com', '');
-
-      await expect(logInPage.EmailField).toBeDisplayed();
-      await expect(logInPage.EmailLabel).toBeDisplayed();
-      await expect(logInPage.EmailInput).toBeDisplayed();
-      await expect(logInPage.EmailError).toBeDisplayed();
-
-      await expect(logInPage.PasswordField).toBeDisplayed();
-      await expect(logInPage.PasswordLabel).toBeDisplayed();
-      await expect(logInPage.PasswordInput).toBeDisplayed();
-      await expect(logInPage.PasswordError).toBeDisplayed();
-    });
-    it('Verify Buttons', async () => {
-      await expect(logInPage.LogInBtn).toBeDisplayed();
-      await expect(logInPage.LogInBtn).toBeClickable();
-      await expect(logInPage.ForgotPassword).toBeDisplayed();
-      await expect(logInPage.ForgotPassword).toBeClickable();
-    });
-
     it('Login Success', async () => {
       browser.refresh();
       await logInPage.login('super.admin@radiumrocket.com', 'Passw0rd1234');
