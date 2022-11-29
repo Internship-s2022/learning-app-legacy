713bce622312c18deb5bcb6ee948514df53abacd
agustinLchazarreta <agustin.chazarreta@radiumrocket.com>
2022-11-29 16:19:39 -0300

    RL-170: Add export selected in the course administration

--- src/components/pages/super-admin/courses/admin/index.tsx
+++ src/components/pages/super-admin/courses/admin/index.tsx
@@ -137,7 +137,12 @@ const AdminCourse = (): JSX.Element => {
   };
 
   const handleExportSelection = (_ids: string[]) => {
-    alert(`Selection (${_ids.length} items): ${_ids}`);
+    const mappedString = _ids.map((e) => `includeIds=${e}&`);
+    const queryString = mappedString.toString().replaceAll(',', '').slice(0, -1);
+    download(
+      `/course-user/export-by-course/csv/${courseId + '?' + queryString}`,
+      'selected-course-users',
+    );
   };
 
   const handleExportTable = () => {
