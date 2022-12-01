39c844e695e7425fede3f8104e75f3fad1a06e61
agustinLchazarreta <agustin.chazarreta@radiumrocket.com>
2022-12-01 16:38:58 -0300

    RL-108: Implement new screen

---
+++ src/components/pages/admin/registration-forms/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegistrationForm = (): JSX.Element => {
+  return (
+    <section>
+      <h2>Admin Landing screen.</h2>
+    </section>
+  );
+};
+
+export default RegistrationForm;
