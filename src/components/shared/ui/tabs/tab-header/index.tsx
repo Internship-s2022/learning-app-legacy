a68e9f7057218a3a068b66dacb28a0ae8c530b52
iaracriscenti <iaracriscenti@gmail.com>
2022-12-02 17:41:50 -0300

    RL-210: Improve tab header logic

--- src/components/shared/ui/tabs/tab-header/index.tsx
+++ src/components/shared/ui/tabs/tab-header/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useEffect, useMemo } from 'react';
+import { Link, useLocation, useParams } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
@@ -9,14 +9,22 @@ import { HeaderTabsProps } from '../types';
 const HeaderTabs = ({ routes }: HeaderTabsProps) => {
   const location = useLocation();
   const [value, setValue] = React.useState(0);
-  const headerRoutes = Object.values(routes).filter((e) => e.enabled == true);
+  const { courseId } = useParams();
+
+  const headerRoutes = useMemo(
+    () => Object.values(routes).filter((e) => e.enabled == true),
+    [routes],
+  );
 
   useEffect(() => {
-    headerRoutes.map((e, index) => {
-      if (location.pathname.includes(e.route)) {
-        setValue(index);
+    const headerIndex = headerRoutes.findIndex((route) => {
+      if (route.route.includes('course/:courseId' && route.route.split('/')[2])) {
+        return location.pathname.includes(route.route.split('/')[2]);
+      } else {
+        return location.pathname.includes(route.route);
       }
     });
+    setValue(headerIndex >= 0 ? headerIndex : 0);
   }, [location.pathname]);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
@@ -38,7 +46,7 @@ const HeaderTabs = ({ routes }: HeaderTabsProps) => {
               data-testid={`tabs-header-${index}`}
               component={Link}
               value={index}
-              to={e.route}
+              to={e.route.replace(':courseId', courseId)}
               label={e.label}
               key={e.label}
             />
