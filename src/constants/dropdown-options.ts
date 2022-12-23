cbe0fd03ac894a2ae6da325dfd5042059afeba79
agustinLchazarreta <agustin.chazarreta@radiumrocket.com>
2022-12-23 11:08:34 -0300

    RL-241: Implement new changes in the add course screen

--- src/constants/dropdown-options.ts
+++ src/constants/dropdown-options.ts
@@ -6,3 +6,13 @@ export const groupTypeOptions: { value: GroupTypes; label: string }[] = [
   { value: 'QA', label: 'QA' },
   { value: 'UIUX', label: 'UIUX' },
 ];
+
+export const courseInternalOptions = [
+  { value: 'true', label: 'Interno' },
+  { value: 'false', label: 'Externo' },
+];
+
+export const courseOptionsTypeOptions = [
+  { value: 'EXPRESS', label: 'Express' },
+  { value: 'FULL', label: 'Full' },
+];
