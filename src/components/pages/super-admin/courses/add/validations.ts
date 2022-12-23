11538c28be1b05e476973bb2c1c891516ff60e12
iaracriscenti <iaracriscenti@gmail.com>
2022-12-23 17:39:42 -0300

    RL-209: Fixed course validations

--- src/components/pages/super-admin/courses/add/validations.ts
+++ src/components/pages/super-admin/courses/add/validations.ts
@@ -10,13 +10,13 @@ const resolverCourse = joiResolver(
     name: Joi.string()
       .pattern(/^(?!\s)(?![\s\S]*\s$)[A-Za-zÀ-ÖØ-öø-ÿ0-9\s()-]+$/)
       .min(3)
-      .max(50)
+      .max(25)
       .required()
       .messages({
         'string.pattern.base':
           'Nombre de curso inválido, no debe empezar o terminar con espacios ni contener símbolos.',
-        'string.min': 'Nombre de curso inválido, debe contener más de 3 letras.',
-        'string.max': 'Nombre de curso inválido, debe contener más de 50 letras.',
+        'string.min': 'Nombre de curso inválido, debe contener más de 3 caracteres.',
+        'string.max': 'Nombre de curso inválido, debe contener más de 25 caracteres.',
         'string.empty': 'Nombre de curso es requerido.',
       }),
     description: Joi.string()
@@ -27,19 +27,26 @@ const resolverCourse = joiResolver(
       .messages({
         'string.pattern.base':
           'Descripción inválida, no debe empezar o terminar con espacios ni contener símbolos determinados.',
-        'string.max': 'Descripción inválida, debe contener menos de 1000 caracteres',
-        'string.min': 'Descripción inválida, debe contener más de 3 caracteres',
-        'string.empty': 'Descripción es un campo requerido',
+        'string.max': 'Descripción inválida, debe contener menos de 1000 caracteres.',
+        'string.min': 'Descripción inválida, debe contener más de 3 caracteres.',
+        'string.empty': 'Descripción es un campo requerido.',
       }),
-    inscriptionStartDate: Joi.date().required().messages({
-      'date.base': 'La fecha es un campo requerido',
+    inscriptionStartDate: Joi.date().greater('11-1-2017').max('11-1-2100').required().messages({
+      'date.base': 'La fecha es un campo requerido.',
+      'date.greater': 'La fecha mínima permitida es 11/01/2017.',
+      'date.max': 'La fecha máxima permitida es 11/01/2100.',
       'any.ref': 'La fecha no debe ser menor a la fecha actual',
     }),
-    inscriptionEndDate: Joi.date().greater(Joi.ref('inscriptionStartDate')).required().messages({
-      'date.greater': 'Debe ser posterior a la fecha de inicio de inscripción',
-      'date.base': 'La fecha es un campo requerido',
-      'any.ref': 'La fecha no debe ser menor a la fecha actual',
-    }),
+    inscriptionEndDate: Joi.date()
+      .greater(Joi.ref('inscriptionStartDate'))
+      .max('11-1-2100')
+      .required()
+      .messages({
+        'date.greater': 'Debe ser posterior a la fecha de inicio de inscripción.',
+        'date.base': 'La fecha es un campo requerido.',
+        'date.max': 'La fecha máxima permitida es 11/01/2100.',
+        'any.ref': 'Debe ser posterior a la fecha de inicio de inscripción.',
+      }),
     startDate: Joi.date()
       .min(
         Joi.ref('inscriptionEndDate', {
@@ -57,21 +64,22 @@ const resolverCourse = joiResolver(
       )
       .required()
       .messages({
-        'date.base': 'La fecha es un campo requerido',
-        'date.max': 'La fecha debe ser un dia posterior al fin de inscripción',
-        'date.min': 'La fecha debe ser un dia posterior al fin de inscripción',
-        'any.ref': 'La fecha no debe ser menor a la fecha actual',
+        'date.base': 'La fecha es un campo requerido.',
+        'date.max': 'La fecha debe ser un dia posterior al fin de inscripción.',
+        'date.min': 'La fecha debe ser un dia posterior al fin de inscripción.',
+        'any.ref': 'La fecha debe ser un dia posterior al fin de inscripción',
       }),
-    endDate: Joi.date().greater(Joi.ref('startDate')).messages({
-      'date.greater': 'Debe ser posterior a la fecha de inicio del curso',
-      'date.base': 'La fecha es un campo requerido',
-      'any.ref': 'La fecha no debe ser menor a la fecha actual',
+    endDate: Joi.date().greater(Joi.ref('startDate')).max('11-1-2100').messages({
+      'date.greater': 'Debe ser posterior a la fecha de inicio del curso.',
+      'date.base': 'La fecha es un campo requerido.',
+      'date.max': 'La fecha máxima permitida es 11/01/2100.',
+      'any.ref': 'Debe ser posterior a la fecha de inicio del curso.',
     }),
     isInternal: Joi.boolean().messages({
-      'string.empty': 'Debe elegir entre Externo o Interno',
+      'string.empty': 'Debe elegir entre Externo o Interno.',
     }),
     type: Joi.string().messages({
-      'string.empty': 'Debe elegir entre Express o Full',
+      'string.empty': 'Debe elegir entre Express o Full.',
     }),
     isActive: Joi.boolean(),
   }),
