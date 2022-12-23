c8da4133f2ed5fb603b29a090ff9d10ff2caf0d2
Guido Cerioni <guido.cerioni@radiumrocket.com>
2022-12-23 18:07:51 -0300

    RL-236: update logo and header styles

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,11 +1,11 @@
 import cardImage from './card-background.png';
 import loadingAnimation from './loading-animation.gif';
 import rocketLogo from './rocket.png';
-import rocketLogoTab from './rocket-tab.png';
+import rocketLogoWhite from './rocket-white.png';
 
 export const images = {
   rocketLogo: { imagePath: rocketLogo, alt: 'Radium Rocket Logo' },
-  rocketLogoTab: { imagePath: rocketLogoTab, alt: 'Radium Rocket Tab Logo' },
+  rocketLogoWhite: { imagePath: rocketLogoWhite, alt: 'Radium Rocket Logo White' },
   loadingAnimation: { imagePath: loadingAnimation, alt: 'Loading animation gif' },
   bgCardImg: { imagePath: cardImage, alt: 'Background Card' },
 };
