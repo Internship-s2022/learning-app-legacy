ad3b3557a5a653d43cee5c32eccac0535fbde3ca
Franco Marini <franco.marini@radiumrocket.com>
2023-01-30 16:02:52 -0300

    RL-XX: Update version and name

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -27,7 +27,6 @@ import {
 Sentry.init({
   dsn: process.env.REACT_APP_SENTRY_DSN,
   environment: process.env.REACT_APP_SHOW_ENV,
-  release: `radium-learning@${process.env.npm_package_version}`,
   integrations: [
     new BrowserTracing({
       routingInstrumentation: Sentry.reactRouterV6Instrumentation(
